0b1441fe0fb3b50e771a40162b962b740e614e4e
xingshuodong <xingshu@deakin.edu.au>
2023-05-30 15:24:38 +1000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My Final Changes');
+  res.send('Just test for triger');
 });
 
 app.listen(PORT, HOST, () => {
